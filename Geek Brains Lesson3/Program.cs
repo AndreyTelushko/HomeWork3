8b4045c5d78a7f56527de3558da211d9c473f52e
Andrey <andreytelushko@gmail.com>
2022-12-24 11:31:36 +0300

    Добавьте файлы проекта.

---
+++ Geek Brains Lesson3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geek_Brains_Lesson3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(PolinomNum(number));
+        }
+        public static string PolinomNum(int number)
+        {
+            if (number.ToString().Length != 5) return "Число не пятизначное";
+            char[] check = number.ToString().ToCharArray();
+            Array.Reverse(check);
+            string result = new string(check);
+            if (result == number.ToString())
+            {
+                return "Да";
+            }
+            else return "Нет";
+        }
+    }
+}
