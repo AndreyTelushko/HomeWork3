aab3b0f11049eff042e2b9c88c15624b84d37276
Andrey <andreytelushko@gmail.com>
2022-12-24 12:26:51 +0300

    Add GetNumber

--- Geek Brains Lesson3/Program.cs
+++ Geek Brains Lesson3/Program.cs
@@ -12,6 +12,7 @@ namespace Geek_Brains_Lesson3
         {
             int number = int.Parse(Console.ReadLine());
             Console.WriteLine(PolinomNum(number));
+            GetNumber();
         }
         public static string PolinomNum(int number)
 
@@ -26,5 +27,31 @@ namespace Geek_Brains_Lesson3
             }
             else return "Нет";
         }
+
+        public static void GetNumber()
+        {
+            Console.WriteLine("Введите координату x1: ");
+            int x1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y1: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату z1: ");
+            int z1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите координату x2: ");
+            int x2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату y2: ");
+            int y2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите координату z2: ");
+            int z2 = int.Parse(Console.ReadLine());
+            double result = 0;                    
+               
+
+            result = Math.Sqrt(Math.Pow(x2 - x1, 2) + (Math.Pow(y2 - y1, 2) + (Math.Pow(z2 - z1, 2))));
+            Console.WriteLine(result);          
+           
+        }
+        
+
+
     }
 }
